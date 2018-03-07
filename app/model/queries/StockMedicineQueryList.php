3a5e74ddbc0e689bc3a614c0fe391fff8573f232
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-03-07 09:03:42 +0100

    queries: new class StockMedicineQueryList
    Pomocna trieda pre tvorbu DQL dotazov na skladovymi zasobami.
    
    Umoznuje skladat filtre, ktore vyberaju len urcite zaznamy,
    nasledne sa tieto filtre aplikuju pri vybere z db.
    
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ app/model/queries/StockMedicineQueryList.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Model\Queries;
+
+use App\Model\Entities\Article;
+use App\Model\Entities\ArticleCategory;
+use App\Model\Entities\StockMedicine;
+use Doctrine\ORM\QueryBuilder;
+use Kdyby;
+use Kdyby\Doctrine\QueryObject;
+
+/**
+ * Trieda, ktora tvori DQL dotazy nad skladovymi zasobami.
+ * @package App\Model\Queries
+ */
+class StockMedicineQueryList extends QueryObject
+{
+	/** @var array Pole filtrov, ktore sa nasledne aplikuju na dotaz */
+	private $filters = array();
+
+	/**
+	 * DQL dotaz na vyber vsetkych skladovych zasob s pozadovanimy filtrami.
+	 * @param \Kdyby\Persistence\Queryable $repository
+	 * @return \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
+	 */
+	protected function doCreateQuery(Kdyby\Persistence\Queryable $repository)
+	{
+		$qb = $repository->createQueryBuilder()
+			->select("sm")
+			->from(StockMedicine::class, "sm");
+
+		foreach ($this->filters as $filter)
+			$filter($qb);
+
+		return $qb;
+	}
+
+	/**
+	 * Filter pre vyber skladovych zasob na zaklade zadaneho lieku.
+	 * @param $id
+	 * @return $this
+	 */
+	public function onlyMedicineSupplies($id)
+	{
+		$this->filters[] = function(QueryBuilder $qb) use ($id) {
+			$qb->where("sm.medicine = :medicine")
+				->setParameter("medicine", $id);
+		};
+
+		return $this;
+	}
+}
