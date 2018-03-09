2f08b51183dcf85e473beb32a6685d53aaceb615
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-03-09 09:11:49 +0100

    forms: SupplierFormFactory: support for editing
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

--- app/forms/SupplierFormFactory.php
+++ app/forms/SupplierFormFactory.php
@@ -69,6 +69,23 @@ class SupplierFormFactory extends BaseFormFactory
 	}
 
 	/**
+	 * Formular pre editaciu dodavatelov.
+	 * @return Form
+	 */
+	public function createEditSupplierForm()
+	{
+		$form = $this->createForm();
+
+		$form->addSubmit("create", "Editovať dodávateľa")
+			->setAttribute('class', 'btn-primary');
+
+		$form->addHidden("supplierId");
+		$form->onSuccess[] = array($this, "editSupplierSubmitted");
+
+		return $form;
+	}
+
+	/**
 	 * Operacia, ktora sa zavola po stlaceni tlacidla na vytvorenie
 	 * dodavatela.
 	 * @param $form
@@ -79,6 +96,20 @@ class SupplierFormFactory extends BaseFormFactory
 	{
 		try {
 			$this->supplierFacade->createSupplier($values);
+		}
+		catch (UniqueConstraintViolationException $ex) {
+			$form->addError("Zadaný dodávateľ už existuje.");
+		}
+	}
+
+	public function editSupplierSubmitted($form, $values)
+	{
+		$supplier = $this->supplierFacade->getSupplier($values->supplierId);
+		if (is_null($supplier))
+			throw new \InvalidArgumentException("Dodávateľ neexistuje.");
+
+		try {
+			$this->supplierFacade->editSupplier($values, $supplier);
 		}
 		catch (UniqueConstraintViolationException $ex) {
 			$form->addError("Zadaný dodávateľ už existuje.");
