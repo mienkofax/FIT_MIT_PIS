93a76904622140605368b1a19a5dfc5929f86155
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-03-21 22:15:00 +0100

    project: script for create zip archive for wis
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ create_zip.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+/bin/rm -rf temp/cache
+/bin/rm -rf temp/proxies
+
+/bin/rm -rf log/error.log
+/bin/rm -rf log/exception*
+
+zip -r xlogin00.zip index.php "Projekt PIS.html" example_import.json doctrine_devel.sql \
+	composer.json .htaccess www tests bin app log temp
